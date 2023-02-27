a1b3fcf3cef0f5300857e43d68a6922f678a81c2
m.mozafari <m.mozafari@DEVELOPER-10>
2023-02-27 12:35:31 +0330

    commit

---
+++ repos/Oshop/Oshop.Dtos/ProductResultDto.cs
@@ -0,0 +1,12 @@
+﻿ 
+namespace Oshop.Dtos
+{
+    public class ProductResultDto
+    {
+        public int ProductId { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoriesId { get; set; }
+    }
+}
--- repos/Oshop/Oshop/Controllers/ProductController.cs
+++ repos/Oshop/Oshop/Controllers/ProductController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Oshop.Data;
 using Oshop.Dtos;
 using Oshop.Entities;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Oshop.Api.Controllers
@@ -18,16 +20,43 @@ namespace Oshop.Api.Controllers
         {
             _db = db;
         }
+        [HttpGet("GetAll")]
+        public async Task<ActionResult> GetAll()
+        {
+            var items = await _db.Products.Select(p => new ProductResultDto
+            {
+                CategoriesId = p.CategoriesId,
+                ImageUrl = p.ImageUrl,
+                Price = p.Price,
+                ProductId = p.Id,
+                Title = p.Title
+            }).ToListAsync();
+            return Ok(items);
+        }
+        [HttpGet("GetById")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var item = await _db.Products.FirstOrDefaultAsync(x => x.Id == id);
+            var product = new ProductResultDto
+            {
+                CategoriesId = item.CategoriesId,
+                Title = item.Title,
+                ImageUrl = item.ImageUrl,
+                Price = item.Price,
+                ProductId = item.Id,
+            };
+            return Ok(product);
+        }
         [HttpPost]
-         
+        [EnableCors("AllowOrigin")]
         public async Task<ActionResult> Create(CreateProductDto model)
         {
             var newProduct = new Product
             {
-             ImageUrl=model.ImageUrl,
-             Price= model.Price,
-             Title=model.Title,
-             CategoriesId=model.CategoriesId,
+                ImageUrl = model.ImageUrl,
+                Price = model.Price,
+                Title = model.Title,
+                CategoriesId = model.CategoriesId,
             };
             await _db.Products.AddAsync(newProduct);
             _db.SaveChanges();
